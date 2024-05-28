21d3d04aca39804a93fe54a80f80dba0fc39a175
vvv9912 <you@example.com>
2024-05-28 21:42:16 +0300

    add authorization

--- internal/Server/handler/auth.go
+++ internal/Server/handler/auth.go
@@ -4,45 +4,9 @@ import (
 	"GophKeeper/pkg/customErrors"
 	"GophKeeper/pkg/logger"
 	"encoding/json"
-	"errors"
 	"go.uber.org/zap"
 	"net/http"
 )
-
-// deferHandler - defer function for get error
-func deferHandler(err error, w http.ResponseWriter) {
-
-	var MyErr *customErrors.CustomError
-	if errors.As(err, &MyErr) {
-
-		if MyErr.StatusCode != 0 {
-
-			w.WriteHeader(MyErr.StatusCode)
-
-			err := json.NewEncoder(w).Encode(RespError{
-				Code:    MyErr.StatusCode,
-				Message: MyErr.Message,
-			})
-			if err != nil {
-				logger.Log.Error("Error writing response", zap.Error(err))
-			}
-
-			return
-		}
-
-	}
-
-	w.WriteHeader(http.StatusInternalServerError)
-
-	err = json.NewEncoder(w).Encode(RespError{
-		Code:    http.StatusInternalServerError,
-		Message: err.Error(),
-	})
-	if err != nil {
-		logger.Log.Error("Error writing response", zap.Error(err))
-	}
-
-}
 
 // HandlerSignUp - регистрация пользователя
 func (h Handler) HandlerSignUp(w http.ResponseWriter, r *http.Request) {
--- internal/Server/handler/service.go
+++ internal/Server/handler/service.go
@@ -2,7 +2,12 @@ package handler
 
 import (
 	"GophKeeper/internal/Server/service"
+	"GophKeeper/pkg/customErrors"
+	"GophKeeper/pkg/logger"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"go.uber.org/zap"
 	"net/http"
 )
 
@@ -10,6 +15,7 @@ type Handler struct {
 	service service.Service
 }
 
+// getUserId - получение id пользователя из контекста request
 func getUserId(r *http.Request) (userId int64, err error) {
 	value := r.Context().Value("UserId")
 
@@ -25,4 +31,39 @@ func getUserId(r *http.Request) (userId int64, err error) {
 	}
 
 	return userId, nil
+}
+
+// deferHandler - defer function for get error
+func deferHandler(err error, w http.ResponseWriter) {
+
+	var MyErr *customErrors.CustomError
+	if errors.As(err, &MyErr) {
+
+		if MyErr.StatusCode != 0 {
+
+			w.WriteHeader(MyErr.StatusCode)
+
+			err := json.NewEncoder(w).Encode(RespError{
+				Code:    MyErr.StatusCode,
+				Message: MyErr.Message,
+			})
+			if err != nil {
+				logger.Log.Error("Error writing response", zap.Error(err))
+			}
+
+			return
+		}
+
+	}
+
+	w.WriteHeader(http.StatusInternalServerError)
+
+	err = json.NewEncoder(w).Encode(RespError{
+		Code:    http.StatusInternalServerError,
+		Message: err.Error(),
+	})
+	if err != nil {
+		logger.Log.Error("Error writing response", zap.Error(err))
+	}
+
 }
