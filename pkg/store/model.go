7a73136785fd73ed98747b79f0ca38b288fd57f4
vvv9912 <you@example.com>
2024-06-01 13:22:32 +0300

    add data

--- pkg/store/model.go
+++ pkg/store/model.go
@@ -5,6 +5,7 @@ import "time"
 type UsersData struct {
 	UserDataId  int64     `db:"user_data_id"`
 	UserId      int64     `db:"user_id"`
+	DataId      int64     `db:"data_id"`
 	DataType    int       `db:"data_type"`
 	Name        string    `db:"name"`
 	Description string    `db:"description"`
--- pkg/store/postgresql/repo.go
+++ pkg/store/postgresql/repo.go
@@ -1,6 +1,7 @@
 package postgresql
 
 import (
+	"GophKeeper/pkg/customErrors"
 	"GophKeeper/pkg/logger"
 	"GophKeeper/pkg/store"
 	"context"
@@ -8,6 +9,7 @@ import (
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	"go.uber.org/zap"
+	"net/http"
 	"time"
 )
 
@@ -57,6 +59,7 @@ func (db *Database) createUserData(ctx context.Context, tx *sql.Tx, userId int64
 	return err
 }
 
+// createData - добавление пользовательских данных.
 func (db *Database) createData(ctx context.Context, tx *sql.Tx, data []byte) (int64, error) {
 
 	query := "INSERT INTO data (encrypt_data) VALUES ($1) RETURNING data_id"
@@ -70,8 +73,9 @@ func (db *Database) createData(ctx context.Context, tx *sql.Tx, data []byte) (in
 	return id, nil
 }
 
+// getDataByUserId - Получение информации о данных пользователя, которые не удалены
 func (db *Database) getDataByUserId(ctx context.Context, userId int64) ([]store.UsersData, error) {
-	query := "SELECT user_data_id, data_id,users_id,data_type,name, description, hash, created_at,update_at,is_deleted FROM users_data WHERE user_id = $1 and is_deleted = false"
+	query := "SELECT user_data_id, data_id,users_id,data_type,name, description, hash, created_at,update_at,is_deleted FROM users_data WHERE user_id = $1 and is_deleted = false FOR UPDATE "
 	row, err := db.db.QueryContext(ctx, query, userId)
 	if err != nil {
 		logger.Log.Error("Error while querying data", zap.Error(err))
@@ -112,9 +116,10 @@ func (db *Database) getDataByUserId(ctx context.Context, userId int64) ([]store.
 	return data, err
 }
 
+// changeData - получение информации об изменненных данных
 func (db *Database) changeData(ctx context.Context, userId int64, lastTimeUpdate time.Time) ([]store.UsersData, error) {
 
-	query := "SELECT user_data_id, name, description,data_type, hash, update_at,is_deleted FROM users_data WHERE user_id = $1 and update_at > $2"
+	query := "SELECT user_data_id, name, description,data_type, hash, update_at,is_deleted FROM users_data WHERE user_id = $1 and update_at > $2 FOR UPDATE "
 
 	row, err := db.db.QueryContext(ctx, query, userId, lastTimeUpdate)
 	if err != nil {
@@ -151,4 +156,76 @@ func (db *Database) changeData(ctx context.Context, userId int64, lastTimeUpdate
 	}
 
 	return data, nil
+}
+
+// updateData - обновление пользовательских данных
+func (db *Database) updateData(ctx context.Context, newData store.UsersData, data []byte) error {
+
+	// Блокирующая транзацкция SELECT * FROM table_name WHERE condition FOR UPDATE;
+	tx, err := db.db.Begin()
+	if err != nil {
+		logger.Log.Error("Error while begin transaction", zap.Error(err))
+		return err
+	}
+
+	queryBlock1 := "SELECT data_id FROM users_data WHERE user_id = $1 and user_data_id = $2 for update"
+	queryBlock2 := "SELECT data_id FROM data WHERE data_id = $1 for update"
+
+	query1 := "UPDATE users_data SET name=$1, description=$2, hash=$3, update_at=$4 WHERE user_data_id = $6"
+	query2 := "UPDATE data SET encrypt_data = $1 where data_id=$2"
+
+	// Получим dataId и заблокируем на изменение табилцу
+	var dataId int64
+	err = tx.QueryRowContext(ctx, queryBlock1, newData.DataId, newData.DataId).Scan(&dataId)
+	if err != nil {
+		logger.Log.Error("Error while querying data", zap.Error(err))
+		return err
+	}
+
+	if dataId != newData.DataId {
+		//todo add err in repo
+		err = customErrors.NewCustomError(nil, http.StatusBadRequest, "dataId != newData.DataId")
+		return err
+	}
+
+	// Заблокируем таблицу data
+	_, err = tx.QueryContext(ctx, queryBlock2, newData.DataId)
+	if err != nil {
+		logger.Log.Error("Error while querying data", zap.Error(err))
+		return err
+	}
+
+	res, err := tx.ExecContext(ctx, query2, data, newData.DataId)
+	if err != nil {
+		logger.Log.Error("Error while querying data", zap.Error(err))
+		return err
+	}
+
+	nr, err := res.RowsAffected()
+	if err != nil {
+		logger.Log.Error("Error while querying data", zap.Error(err))
+		return err
+	}
+	if nr == 0 {
+		//todo add err in repo
+		return customErrors.NewCustomError(nil, http.StatusNotFound, "data not found")
+	}
+
+	res, err = tx.ExecContext(ctx, query1, newData.Name, newData.Description, newData.Hash, newData.UpdateAt, newData.UserDataId)
+	if err != nil {
+		logger.Log.Error("Error while querying data", zap.Error(err))
+		return err
+	}
+
+	nr, err = res.RowsAffected()
+	if err != nil {
+		logger.Log.Error("Error while querying data", zap.Error(err))
+		return err
+	}
+	if nr == 0 {
+		//todo add err in repo
+		return customErrors.NewCustomError(nil, http.StatusNotFound, "data not found")
+	}
+
+	return nil
 }
