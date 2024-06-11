a28e65388b8f813afda8c93da89969d55f3957a6
VETaskanov <VETaskanov@get-lab.ru>
2024-06-11 15:54:46 +0300

    add encrypt data

---
+++ internal/Agent/Encrypt/encrypt_test.go
@@ -0,0 +1,19 @@
+package Encrypt
+
+import (
+	"crypto/rand"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestEncrypt_Encrypt(t *testing.T) {
+	key := make([]byte, 32)
+	rand.Read(key)
+	encr := NewEncrypt(key)
+	e, err := encr.Encrypt([]byte("hello world"))
+	require.NoError(t, err)
+	d, err := encr.Decrypt(e)
+	require.NoError(t, err)
+	require.Equal(t, []byte("hello world"), d)
+
+}
