d2f09d277c052e16b3f48dc0e2c8137c72dc27e8
vvv9912 <you@example.com>
2024-06-13 22:01:02 +0300

    fix after the review

--- internal/Agent/service/binaryData.go
+++ internal/Agent/service/binaryData.go
@@ -22,110 +22,40 @@ func (s *Service) CreateBinaryFile(ctx context.Context, path string, name, descr
 	}
 
 	// Создаем новый шифрованный файл в tmp папке
-	// Имя нового файла
-	newNameFile := uuid.NewString()
-	//Полный путь к новому файлу
-	pathTmp := path2.Join(PathTmp, newNameFile)
-
-	err := s.Encrypter.EncryptFile(path, pathTmp)
+	pathTmpFile, err := s.createEncryptedFile(path)
 	if err != nil {
 		return err
 	}
 
+	// Очистка из tmp папки
 	defer func() {
-		os.Remove(pathTmp)
+		os.Remove(pathTmpFile)
 	}()
-
-	// Распределение на чанки
-	r := NewReader(pathTmp)
-	// Количество чанков в файле
-	n, err := r.NumChunk()
-
-	if err != nil {
-		return err
-	}
 
 	// Получаем оригинальное название файла пользователя
 	_, originalFileName := path2.Split(path)
 
-	// id - чанка при передачи
-	var uuidChunk string
+	// Распределение на чанки
+	r := NewReader(pathTmpFile)
 
-	// Данные о файле
-	infoOriginalFile := server.DataFileInfo{OriginalFileName: originalFileName}
-
-	// Метаданные оригинального файла
-	dataOriginalFile, err := json.Marshal(infoOriginalFile)
+	// Количество чанков в файле
+	n, err := r.NumChunk()
 	if err != nil {
-		logger.Log.Error("Marshal json failed", zap.Error(err))
 		return err
 	}
 
-	// Структура запроса с данными о файле на сервере
-	reqData := &server.ReqData{
-		Name:        name,
-		Description: description,
-		Data:        dataOriginalFile,
-	}
-
-	// Шифруем данные о файле
-	if err := s.encryptData(reqData); err != nil {
-		return err
-	}
-
-	reqDataJson, err := json.Marshal(reqData)
+	// Подготовка данных для запроса на сервер
+	reqData, reqDataJson, err := s.prepareReqBinaryFile(originalFileName, name, description)
 	if err != nil {
-		logger.Log.Error("Marshal json failed", zap.Error(err))
 		return err
 	}
-
-	var resp *server.RespData
-
-	// Передача файла на сервер
-	for i := 1; i <= n; i++ {
-		// Считываем файл по чанкам
-		data, err := r.ReadFile(i)
-		if err != nil {
-			return err
-		}
-
-		maxChunk := r.SizeChunk * (i)
-		if i == n {
-			maxChunk = int(r.Size())
-		}
-
-		uuidChunk, resp, err = s.PostCrateFileStartChunks(ctx, data, r.NameFile, uuidChunk, int(r.SizeChunk)*(i-1), maxChunk, int(r.Size()), reqDataJson)
-		if err != nil {
-			logger.Log.Error("PostCrateFileStartChunks failed", zap.Error(err))
-			return err
-		}
-
-		// Вывод полезной информации пользователю
-		ch <- fmt.Sprintf("Передано кБайт %0.1f из %0.1f", float64(maxChunk)/1024.0, float64(r.Size())/1024)
-
-	}
-	if resp == nil {
-		err := fmt.Errorf("resp is nil")
-		logger.Log.Error("resp is nil", zap.Error(err))
+	// Передача на сервер
+	resp, err := s.transferDataBinaryFile(ctx, r, n, reqDataJson, ch)
+	if err != nil {
 		return err
 	}
-
-	// Копируем файл в локальное хранилище Агента
-	NewNameFile := uuid.NewString()
-	if err := copyFile(path, PathStorage, NewNameFile); err != nil {
-		logger.Log.Error("copyFile failed", zap.Error(err))
-		return err
-	}
-	// Новое поле с мета данными, тк путь и названия файла поменялись
-	metaData := &store.MetaData{
-		FileName: NewNameFile,
-		Size:     r.size,
-		PathSave: PathStorage,
-	}
-
-	// Сохранение в локальное хранилище
-	if err := s.StorageData.CreateBinaryFile(ctx, dataOriginalFile, resp.UserDataId, name, description, resp.Hash, resp.CreatedAt, resp.UpdateAt, metaData); err != nil {
-		logger.Log.Error("CreateBinaryFile failed", zap.Error(err))
+	// сохранение в локальный репозиторий
+	if err = s.saveLocalFile(ctx, r, pathTmpFile, name, description, reqData, resp); err != nil {
 		return err
 	}
 
@@ -203,6 +133,7 @@ func (s *Service) UpdateBinaryFile(ctx context.Context, path string, userDataId 
 		ch <- fmt.Sprintf("Передано кБайт %0.1f из %0.1f", float64(maxChunk)/1024.0, float64(r.Size())/1024)
 
 	}
+	//todo удаление предыдущего файла
 
 	// Копирование к себе в хранилище
 	NewNameFile := uuid.NewString()
@@ -233,6 +164,111 @@ func (s *Service) UpdateBinaryFile(ctx context.Context, path string, userDataId 
 		return err
 	}
 
+	return nil
+}
+
+func (s *Service) createEncryptedFile(path string) (string, error) {
+	// Создаем новый шифрованный файл в tmp папке
+	// Имя нового файла
+	newNameFile := uuid.NewString()
+	//Полный путь к новому файлу
+	pathTmp := path2.Join(PathTmp, newNameFile)
+
+	err := s.Encrypter.EncryptFile(path, pathTmp)
+	if err != nil {
+		return "", err
+	}
+
+	return pathTmp, nil
+}
+func (s *Service) prepareReqBinaryFile(originalFileName string, name, description string) (*server.ReqData, []byte, error) {
+
+	// Данные о файле
+	infoOriginalFile := server.DataFileInfo{OriginalFileName: originalFileName}
+
+	// Метаданные оригинального файла
+	dataOriginalFile, err := json.Marshal(infoOriginalFile)
+	if err != nil {
+		logger.Log.Error("Marshal json failed", zap.Error(err))
+		return nil, nil, err
+	}
+
+	// Структура запроса с данными о файле на сервере
+	reqData := &server.ReqData{
+		Name:        name,
+		Description: description,
+		Data:        dataOriginalFile,
+	}
+
+	// Шифруем данные о файле
+	if err := s.encryptData(reqData); err != nil {
+		return nil, nil, err
+	}
+
+	reqDataJson, err := json.Marshal(reqData)
+	if err != nil {
+		logger.Log.Error("Marshal json failed", zap.Error(err))
+		return nil, nil, err
+	}
+	return reqData, reqDataJson, nil
+
+}
+
+func (s *Service) transferDataBinaryFile(ctx context.Context, r *Reader, n int, reqDataJson []byte, ch chan<- string) (*server.RespData, error) {
+	var resp *server.RespData
+
+	var uuidChunk string
+	// Передача файла на сервер
+	for i := 1; i <= n; i++ {
+		// Считываем файл по чанкам
+		data, err := r.ReadFile(i)
+		if err != nil {
+			return nil, err
+		}
+
+		maxChunk := r.SizeChunk * (i)
+		if i == n {
+			maxChunk = int(r.Size())
+		}
+
+		uuidChunk, resp, err = s.PostCrateFileStartChunks(ctx, data, r.NameFile, uuidChunk, int(r.SizeChunk)*(i-1), maxChunk, int(r.Size()), reqDataJson)
+		if err != nil {
+			logger.Log.Error("PostCrateFileStartChunks failed", zap.Error(err))
+			return nil, err
+		}
+
+		// Вывод полезной информации пользователю
+		ch <- fmt.Sprintf("Передано кБайт %0.1f из %0.1f", float64(maxChunk)/1024.0, float64(r.Size())/1024)
+
+	}
+	if resp == nil {
+		err := fmt.Errorf("resp is nil")
+		logger.Log.Error("resp is nil", zap.Error(err))
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (s *Service) saveLocalFile(ctx context.Context, r *Reader, pathTmpFile, name, description string, reqData *server.ReqData, resp *server.RespData) error {
+	// Копируем файл в локальное хранилище Агента
+	NewNameFile := uuid.NewString()
+	if err := copyFile(pathTmpFile, PathStorage, NewNameFile); err != nil {
+		logger.Log.Error("copyFile failed", zap.Error(err))
+		return err
+	}
+	// Новое поле с мета данными, тк путь и названия файла поменялись
+	metaData := &store.MetaData{
+		FileName: NewNameFile,
+		Size:     r.size,
+		PathSave: PathStorage,
+	}
+
+	// Сохранение в локальное хранилище
+	if err := s.StorageData.CreateBinaryFile(ctx, reqData.Data, resp.UserDataId, name, description, resp.Hash, resp.CreatedAt, resp.UpdateAt, metaData); err != nil {
+		logger.Log.Error("CreateBinaryFile failed", zap.Error(err))
+		return err
+	}
 	return nil
 }
 
