f6a21dc28c2be519f98168be8ffdc6c5907d8edd
vvv9912 <you@example.com>
2024-06-20 21:54:05 +0300

    fix after the review

--- pkg/store/postgresql/user.go
+++ pkg/store/postgresql/user.go
@@ -29,6 +29,14 @@ func (db *Database) CreateUser(ctx context.Context, login, password string) (int
 	return id, nil
 }
 
+// todo
 func (db *Database) GetUserId(ctx context.Context, login, password string) (int64, error) {
+	//row := db.db.QueryRowxContext(ctx, "SELECT user_id FROM users WHERE login = $1 AND password = $2", login, password)
+	//var id int64
+	//err := row.Scan(&id)
+	//if err != nil {
+	//	logger.Log.Error("Error while getting user id", zap.String("login", login), zap.String("password", password), zap.Error(err))
+	//	return 0, err
+	//}
 	return 1, nil
 }
