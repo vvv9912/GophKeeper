02cfc81eb6b39801828950479fd56bf6f114e8f7
vvv9912 <you@example.com>
2024-06-23 15:39:36 +0300

    add test

---
+++ internal/Agent/command/auth_test.go
@@ -0,0 +1,85 @@
+package command
+
+import (
+	"GophKeeper/internal/Agent/service"
+	mock_service "GophKeeper/internal/Agent/service/mocks"
+	"context"
+	"fmt"
+	"github.com/golang/mock/gomock"
+	"github.com/spf13/cobra"
+	"testing"
+)
+
+func TestSuccessfulSignInWithValidCredentials(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUse := mock_service.NewMockUseCaser(ctrl)
+
+	serv := service.Service{mockUse}
+
+	c := NewCobra(&serv)
+
+	args := []string{"validUser", "validPass"}
+
+	mockUse.EXPECT().SignIn(gomock.Any(), "validuser", "validPass").Return("validJWT", nil)
+	cobr := cobra.Command{}
+	cobr.SetContext(context.Background())
+
+	c.SignIn(&cobr, args)
+}
+func TestSignInWithValidCredentials(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUse := mock_service.NewMockUseCaser(ctrl)
+
+	serv := service.Service{mockUse}
+
+	c := NewCobra(&serv)
+
+	args := []string{"validUser", "validPass"}
+
+	mockUse.EXPECT().SignIn(gomock.Any(), "validuser", "validPass").Return("validJWT", fmt.Errorf("error"))
+	cobr := cobra.Command{}
+	cobr.SetContext(context.Background())
+
+	c.SignIn(&cobr, args)
+}
+
+func TestCobra_SignUp(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUse := mock_service.NewMockUseCaser(ctrl)
+
+	serv := service.Service{mockUse}
+
+	c := NewCobra(&serv)
+
+	args := []string{"validUser", "validPass"}
+
+	mockUse.EXPECT().SignUp(gomock.Any(), "validuser", "validPass").Return("validJWT", fmt.Errorf("error"))
+	cobr := cobra.Command{}
+	cobr.SetContext(context.Background())
+
+	c.SignUp(&cobr, args)
+}
+func TestCobra_SignUp2(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUse := mock_service.NewMockUseCaser(ctrl)
+
+	serv := service.Service{mockUse}
+
+	c := NewCobra(&serv)
+
+	args := []string{"validUser", "validPass"}
+
+	mockUse.EXPECT().SignUp(gomock.Any(), "validuser", "validPass").Return("validJWT", nil)
+	cobr := cobra.Command{}
+	cobr.SetContext(context.Background())
+
+	c.SignUp(&cobr, args)
+}
