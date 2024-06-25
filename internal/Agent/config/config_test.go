27d686739d43279b633f637e626283be93425a93
vvv9912 <you@example.com>
2024-06-25 21:24:31 +0300

    add test

---
+++ internal/Agent/config/config_test.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetConfig(t *testing.T) {
+
+	cfg := Config{}
+	cfg.setDefaultValues()
+
+	retrievedConfig := cfg.Get()
+
+	assert.Equal(t, ":8080", retrievedConfig.ServerDNS)
+	assert.Equal(t, "agent/cert.pem", retrievedConfig.CertFile)
+	assert.Equal(t, "agent/key.pem", retrievedConfig.KeyFile)
+	assert.Equal(t, "clientdb.db", retrievedConfig.PathDatabaseFile)
+	assert.Equal(t, "FileAgent/tmp", retrievedConfig.PathTmpStorage)
+	assert.Equal(t, "FileAgent/storage", retrievedConfig.PathLocalStorage)
+	assert.Equal(t, "FileAgent/userData", retrievedConfig.PathUserData)
+	assert.Equal(t, "12345678901234567890123456789012", retrievedConfig.PathSecretKey)
+	assert.Equal(t, "debug", retrievedConfig.LevelLogger)
+}
