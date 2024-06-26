ac507843d165296abd12547cfb8ce624dd7f30a0
vvv9912 <you@example.com>
2024-06-27 01:02:17 +0300

    add test for handler

--- internal/Server/handler/data_test.go
+++ internal/Server/handler/data_test.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"mime/multipart"
@@ -15,6 +16,7 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestHandler_HandlerPostCredentials(t *testing.T) {
@@ -585,4 +587,170 @@ func TestHandler_HandlerGetListDataBadGetList(t *testing.T) {
 	h.HandlerGetListData(w, req)
 
 	assert.Equal(t, http.StatusInternalServerError, w.Code)
+}
+
+//HandlerCheckUpdateData
+
+func TestHandler_HandlerCheckUpdateData(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	u := mock_service.NewMockUseCaser(ctrl)
+	s := service2.Service{
+		UseCaser: u,
+	}
+
+	u.EXPECT().ChangeData(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return([]byte("data"), nil)
+
+	req := httptest.NewRequest(http.MethodPost, "/CheckUpdate", nil)
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "UserId", int64(1))
+
+	req = req.WithContext(ctx)
+	timee := time.Now()
+	req.Header.Set("Last-Time-Update", timee.Format("2006-01-02 15:04:05.999999"))
+	chiCtx := chi.NewRouteContext()
+	chiCtx.URLParams.Add("userDataId", "123")
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, chiCtx))
+	w := httptest.NewRecorder()
+
+	h := Handler{service: &s}
+	h.HandlerCheckUpdateData(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+func TestHandler_HandlerCheckUpdateDataBadUserId(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	u := mock_service.NewMockUseCaser(ctrl)
+	s := service2.Service{
+		UseCaser: u,
+	}
+
+	//u.EXPECT().ChangeData(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return([]byte("data"), nil)
+
+	req := httptest.NewRequest(http.MethodPost, "/CheckUpdate", nil)
+
+	timee := time.Now()
+	req.Header.Set("Last-Time-Update", timee.Format("2006-01-02 15:04:05.999999"))
+	chiCtx := chi.NewRouteContext()
+	chiCtx.URLParams.Add("userDataId", "123")
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, chiCtx))
+	w := httptest.NewRecorder()
+
+	h := Handler{service: &s}
+	h.HandlerCheckUpdateData(w, req)
+
+	assert.Equal(t, http.StatusInternalServerError, w.Code)
+}
+func TestHandler_HandlerCheckUpdateDataBadTimeGet(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	u := mock_service.NewMockUseCaser(ctrl)
+	s := service2.Service{
+		UseCaser: u,
+	}
+
+	req := httptest.NewRequest(http.MethodPost, "/CheckUpdate", nil)
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "UserId", int64(1))
+
+	req = req.WithContext(ctx)
+
+	chiCtx := chi.NewRouteContext()
+	chiCtx.URLParams.Add("userDataId", "123")
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, chiCtx))
+	w := httptest.NewRecorder()
+
+	h := Handler{service: &s}
+	h.HandlerCheckUpdateData(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+func TestHandler_HandlerCheckUpdateDataBadTimeParse(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	u := mock_service.NewMockUseCaser(ctrl)
+	s := service2.Service{
+		UseCaser: u,
+	}
+
+	req := httptest.NewRequest(http.MethodPost, "/CheckUpdate", nil)
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "UserId", int64(1))
+
+	req = req.WithContext(ctx)
+	timee := time.Now()
+
+	req.Header.Set("Last-Time-Update", timee.String())
+	chiCtx := chi.NewRouteContext()
+	chiCtx.URLParams.Add("userDataId", "123")
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, chiCtx))
+	w := httptest.NewRecorder()
+
+	h := Handler{service: &s}
+	h.HandlerCheckUpdateData(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+func TestHandler_HandlerCheckUpdateDataBadUserDataId(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	u := mock_service.NewMockUseCaser(ctrl)
+	s := service2.Service{
+		UseCaser: u,
+	}
+
+	req := httptest.NewRequest(http.MethodPost, "/CheckUpdate", nil)
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "UserId", int64(1))
+
+	req = req.WithContext(ctx)
+	timee := time.Now()
+	req.Header.Set("Last-Time-Update", timee.Format("2006-01-02 15:04:05.999999"))
+
+	w := httptest.NewRecorder()
+
+	h := Handler{service: &s}
+	h.HandlerCheckUpdateData(w, req)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+func TestHandler_HandlerCheckUpdateDataBadChangeData(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	u := mock_service.NewMockUseCaser(ctrl)
+	s := service2.Service{
+		UseCaser: u,
+	}
+
+	u.EXPECT().ChangeData(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, fmt.Errorf("error"))
+
+	req := httptest.NewRequest(http.MethodPost, "/CheckUpdate", nil)
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "UserId", int64(1))
+
+	req = req.WithContext(ctx)
+	timee := time.Now()
+	req.Header.Set("Last-Time-Update", timee.Format("2006-01-02 15:04:05.999999"))
+	chiCtx := chi.NewRouteContext()
+	chiCtx.URLParams.Add("userDataId", "123")
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, chiCtx))
+	w := httptest.NewRecorder()
+
+	h := Handler{service: &s}
+	h.HandlerCheckUpdateData(w, req)
+
+	assert.Equal(t, http.StatusInternalServerError, w.Code)
+}
+
+//HandlerCheckChanges
+
+func TestHandler_HandlerCheckChanges(t *testing.T) {
+
 }
